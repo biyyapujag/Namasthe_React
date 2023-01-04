a4b00861e4d5be697684e39abc66b8feec337597
Jagadeesh Biyyapu <v-jbiyyapu@microsoft.com>
2023-01-04 10:36:14 +0530

    first commit

---
+++ Workspace_React/App.js
@@ -0,0 +1,7 @@
+
+    const heading1 = React.createElement("h1",{id:'title'},"Namaste Everyone");
+    const heading2 = React.createElement("h1",{id:'title'},"Namaste Everyone");
+
+    const root = ReactDOM.createRoot(document.getElementById("root"));
+    root.render(React.createElement("div",{},[heading1,heading2]));
+    
