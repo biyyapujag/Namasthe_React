4d28bf8c8657107f10759f47dc515710db1c0716
Jagadeesh Biyyapu <v-jbiyyapu@microsoft.com>
2023-01-06 21:09:27 +0530

    test

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
-
-    const heading1 = React.createElement("h1",{id:'title'},"Namaste Everyone");
+import React from 'react';
+import  ReactDOM  from 'react-dom/client';
+    const heading1 = React.createElement("h1",{id:'title parcel'},"Namaste parcel");
     const heading2 = React.createElement("h1",{id:'title'},"Namaste Everyone");
 
     const root = ReactDOM.createRoot(document.getElementById("root"));
